918c1c342aa137901f9a069973827207f0c1a30c
169408 <ruslanzhukotynskyi@gmail.com>
2026-07-08 12:55:44 +0200

    refactor(docker): improve development environment

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+if [ ! -d "vendor" ]; then
+    echo "Installing Composer dependencies..."
+    composer install --prefer-dist --no-interaction --optimize-autoloader
+fi
+
+exec php-fpm
